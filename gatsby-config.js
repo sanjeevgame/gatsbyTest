2421ba03a4b994219e722fe8eeaf05769cf6db05
Sanjeev <sanjeev@jungleegames.com>
2019-05-20 12:12:44 +0530

    setup repo

---
+++ gatsby-config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  siteMetadata: {
+    title: `JR static page`,
+    author: `Sanjeev`,
+    description: `A starter landing demonstrating what Gatsby can do.`,
+    siteUrl: `https://www.google.com/`,
+    social: {
+      twitter: `sanjeevandindia`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/pages`,
+        name: `page`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `JWR static pages`,
+        short_name: `JWR`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
---
+++ src/components/layout.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { Link } from "gatsby"
+//import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    // const rootPath = `${__PATH_PREFIX__}/`
+
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          //  maxWidth: rhythm(24),
+          //padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>JWR Static Pages</header>
+        <section>
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/page-1`}
+                >
+                  <a>Page 1</a>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/page-2`}
+                >
+                  <a>Page 2</a>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/page-3`}
+                >
+                  <a>Page 3</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <article>{children}</article>
+        </section>
+
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
